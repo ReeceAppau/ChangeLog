912ea41952d03f1a490c6239485a1f0a2e9b4578
Reece Appau <reeceappau.ma@gmail.com>
2023-02-05 09:53:58 +0000

    Added initial routes

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import express from 'express'
+import router from './router'
 
 const app = express()
 
 app.get('/', (req, res) => {
     console.log("Hello from express")
 })
+
+app.use('/api', router)
 
 export default app
